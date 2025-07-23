6165522e26f280d8852dc19de3bc2087588a2ce8
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2025-07-23 17:56:39 +0530

    models

--- rahul/accounts/models.py
+++ rahul/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    BRANCH_CHOICE = [
+        ("CS" , "Computer Science"),
+        ("AI" , "Artificial Intelligance"),
+        ("DS" , "Data Science"),
+    ]
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250)
+    age = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+    branch = models.CharField(max_length=20, choices=BRANCH_CHOICE)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.branch}"
--- rahul/accounts/views.py
+++ rahul/accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 # Create your views here.
 def login(request):
-    return HttpResponse("LOGIN Page")
+    return render(request,'account.html')
+
+def students(request):
+    students = Student.objects.all()
+    return render(request,'student_list.html',{"student":students})
